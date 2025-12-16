c2e63ca19c87f1161941576975c5265087779c76
Daniel Shefer <dandanshefer@gmail.com>
2025-12-16 22:28:42 +0200

    fixed fetch_usable to ignore the last entry (64 bit mode)

--- paging.h
+++ paging.h
@@ -10,7 +10,8 @@ E820Entry* fetch_usable_memory(E820Info* info, E820Entry* usable_entries);
 uint32_t num_usable_entries(E820Info* info) {
     uint32_t count = 0;
     for (size_t i = 0; i < info->num_entries; ++i) {
-        if (info->entries[i].type == 1 && (info->entries[i].length_low > 1024 * 4 || info->entries[i].length_high > 0) && !(info->entries[i].base_low == 0 && info->entries[i].base_high == 0)) { // Type 1 indicates usable RAM
+        if (info->entries[i].type == 1 && (info->entries[i].length_low > 1024 * 4 && info->entries[i].length_high == 0)
+         && info->entries[i].base_high == 0) { // Type 1 indicates usable RAM
             count++;
         }
     }
@@ -20,7 +21,8 @@ uint32_t num_usable_entries(E820Info* info) {
 E820Entry* fetch_usable_memory(E820Info* info, E820Entry* usable_entries) {
     uint32_t index = 0;
     for (size_t i = 0; i < info->num_entries; ++i) {
-        if (info->entries[i].type == 1 && (info->entries[i].length_low > 1024 * 4 || info->entries[i].length_high > 0) && !(info->entries[i].base_low == 0 && info->entries[i].base_high == 0)) {
+        if (info->entries[i].type == 1 && (info->entries[i].length_low > 1024 * 4 && info->entries[i].length_high == 0)
+         && info->entries[i].base_high == 0) {
             usable_entries[index++] = info->entries[i];
         }
     }
