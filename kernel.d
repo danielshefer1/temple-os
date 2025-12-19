e4c4635d18603d65e05a72b13265ea3b92982dfa
Daniel Shefer <dandanshefer@gmail.com>
2025-12-20 00:44:06 +0200

    Removed cached .d files

--- kernel.d
+++
@@ -1,5 +0,0 @@
-kernel.o: kernel.c print_text.h E820.h paging.h data_structs.h
-print_text.h:
-E820.h:
-paging.h:
-data_structs.h:
--- paging.d
+++
@@ -1,5 +0,0 @@
-paging.o: paging.c paging.h E820.h print_text.h data_structs.h
-paging.h:
-E820.h:
-print_text.h:
-data_structs.h:
