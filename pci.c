7a569d2ebacad564ae92fa47d1763c40c8017c6a
Daniel Shefer <dandanshefer@gmail.com>
2026-01-31 03:23:32 +0200

    just changed vendor to display in hex

--- pci.c
+++ pci.c
@@ -4,5 +4,5 @@ void InitPci() {
     FillPageDirectoryPCI(ecam_ptr, PAGE_SIZE);
     ecam_ptr = (pci_config_t*)(((uint32_t)ecam_ptr) + PCI_OFFSET);
     uint16_t vendor_id = ecam_ptr->vendor_id;
-    kprintf("First Vendor ID: %d", vendor_id);
+    kprintf("First Vendor ID: %x", vendor_id);
 }
